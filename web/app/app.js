116bc9592f28a99fdeef8411bac9c884f2290fb1
phongnhung <appstoregugle2310@gmail.com>
2015-09-22 21:52:27 +0700

    Laravel - add source

---
+++ web/app/app.js
@@ -0,0 +1,15 @@
+/**
+ * @author: PVLam
+ * @createDate: May 19, 2015
+ * @module: app
+ */
+(function(){
+    'use strict';
+    var app = angular.module("app", [
+        //add required libs in here
+    ]);
+    app.config({
+
+    });
+
+});
